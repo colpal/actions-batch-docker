8aca0c890755af8e5cae1e46aa06e94a7592f3a7
Don Kim <41008205+GoodToast@users.noreply.github.com>
2020-07-01 10:38:53 -0400

    Update input variable names

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,17 +2,17 @@
 
 set -eux
 
-gcloud container clusters get-credentials "$CLUSTER" \
-  --project "$PROJECT" \
-  --zone "$ZONE"
+gcloud container clusters get-credentials "$INPUT_CLUSTER" \
+  --project "$INPUT_PROJECT" \
+  --zone "$INPUT_ZONE"
 kubectl config set-context \
   --current \
-  --namespace="$NAMESPACE"
+  --namespace="$INPUT_NAMESPACE"
 
 dag_directory=/exports/dags
 pod_name=$(\
   kubectl get pod \
-    -l "$POD_LABEL" \
+    -l "$INPUT_POD_LABEL" \
     -o jsonpath='{.items[0].metadata.name}'\
 )
 kubectl exec "$pod_name" -- sh -c "rm -rf $dag_directory/*"
