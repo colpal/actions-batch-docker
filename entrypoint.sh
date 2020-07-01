4e3d3943f9b9e88b1b672580e4cc6120e0419bfe
Don Kim <41008205+GoodToast@users.noreply.github.com>
2020-07-01 10:18:44 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,22 @@
 #!/bin/sh
 
-set -ev
+set -eux
 
-# Write code here
+gcloud container clusters get-credentials "$CLUSTER" \
+  --project "$PROJECT" \
+  --zone "$ZONE"
+kubectl config set-context \
+  --current \
+  --namespace="$NAMESPACE"
+
+dag_directory=/exports/dags
+pod_name=$(\
+  kubectl get pod \
+    -l "$POD_LABEL" \
+    -o jsonpath='{.items[0].metadata.name}'\
+)
+kubectl exec "$pod_name" -- sh -c "rm -rf $dag_directory/*"
+for dag_file in *.py; do
+  test ! -f "$dag_file" \
+    || kubectl cp "$dag_file" "$pod_name:$dag_directory"
+done
