36d206ee2f0dff96f120d17ff6a0aa56f0582969
Nicholas Farley <nicholasafarley@gmail.com>
2020-07-08 15:22:42 -0400

    Add rough draft for entrypoint

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const path = require('path');
+
+const core = require('@actions/core');
+
+(async () => {
+  const rootDirectory = core.getInput('root-directory', { required: true });
+  const changedFiles = core.getInput('changed-files', { required: true });
+
+  const changedDirectories = new Set(changedFiles
+    .map((p) => path.relative(rootDirectory, p))
+    .filter((p) => !p.startsWith('../'))
+    .map((p) => path.parse(p).root));
+
+  console.log(changedDirectories);
+})();
