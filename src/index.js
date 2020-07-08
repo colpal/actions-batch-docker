af7981b3811054e718b51449425a678f37dc650b
Nicholas Farley <nicholasafarley@gmail.com>
2020-07-08 15:42:22 -0400

    Sprinkle console.logs tastefully

--- src/index.js
+++ src/index.js
@@ -6,10 +6,10 @@ const core = require('@actions/core');
   const rootDirectory = core.getInput('root-directory', { required: true });
   const changedFiles = JSON.parse(core.getInput('changed-files', { required: true }));
 
-  const changedDirectories = new Set(changedFiles
-    .map((p) => path.relative(rootDirectory, p))
-    .filter((p) => !p.startsWith('../'))
-    .map((p) => path.parse(p).root));
-
+  const relativeChanged = changedFiles.map((p) => path.relative(rootDirectory, p));
+  console.log(relativeChanged);
+  const relevantChanged = relativeChanged.filter((p) => !p.startsWith('../'));
+  console.log(relevantChanged);
+  const changedDirectories = relevantChanged.map((p) => path.parse(p).root);
   console.log(changedDirectories);
 })();
