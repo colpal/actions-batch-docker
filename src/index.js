7557c2605bad532e1bcdbd99048d006f931597bc
Nicholas Farley <nicholasafarley@gmail.com>
2020-07-10 11:51:10 -0400

    Print out dockerfile paths

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 const path = require('path');
 
 const core = require('@actions/core');
+const glob = require('@actions/glob');
 
 (async () => {
   const rootDirectory = core.getInput('root-directory', { required: true });
   const changedFiles = core.getInput('changed-files', { required: true });
+  const dockerfiles = await glob
+    .create(path.resolve(rootDirectory, '**', 'Dockerfile.*'))
+    .then((globber) => globber.glob());
   const contexts = JSON
     .parse(changedFiles)
     .map((p) => path.relative(rootDirectory, p))
     .filter((p) => !p.startsWith('../'))
     .map((p) => p.split('/')[0]);
-  const uniqueContexts = [...new Set(contexts)];
 
-  console.log(uniqueContexts);
+  console.log(dockerfiles);
+  console.log(contexts);
 })();
