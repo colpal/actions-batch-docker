6f67fc790d657437415b8d6177c5b55213764eb5
Nicholas Farley <nicholasafarley@gmail.com>
2020-07-10 13:17:55 -0400

    Fail job on error

--- src/index.js
+++ src/index.js
@@ -14,33 +14,37 @@ function includesBy(set, fn) {
 }
 
 (async () => {
-  const project = core.getInput('project', { required: true });
-  const rootDirectory = core.getInput('root-directory', { required: true });
-  const changedFiles = core.getInput('changed-files', { required: true });
+  try {
+    const project = core.getInput('project', { required: true });
+    const rootDirectory = core.getInput('root-directory', { required: true });
+    const changedFiles = core.getInput('changed-files', { required: true });
 
-  const relevantChanges = new Set(
-    JSON
-      .parse(changedFiles)
-      .map((p) => path.relative(rootDirectory, p))
-      .filter((p) => !p.startsWith('../')),
-  );
+    const relevantChanges = new Set(
+      JSON
+        .parse(changedFiles)
+        .map((p) => path.relative(rootDirectory, p))
+        .filter((p) => !p.startsWith('../')),
+    );
 
-  const globber = await glob.create(path.resolve(rootDirectory, '**', 'Dockerfile.*'));
-  const matches = await globber.glob();
+    const globber = await glob.create(path.resolve(rootDirectory, '**', 'Dockerfile.*'));
+    const matches = await globber.glob();
 
-  const dockerfiles = matches
-    .filter((file) => {
-      const dirname = path.dirname(path.relative(rootDirectory, file));
-      return includesBy(relevantChanges, (change) => change.startsWith(dirname));
-    })
-    .map(async (file) => {
-      const filename = path.basename(file);
-      const image = filename.match(/^Dockerfile\.(.*)$/)[1];
-      const gitSHA = process.env.GITHUB_SHA;
-      const tag = `gcr.io/${project}/${image}:${gitSHA}`;
-      const cwd = path.dirname(file);
-      await exec('docker', ['build', '-f', filename, '-t', tag], { cwd });
-    });
-  await Promise.all(dockerfiles);
-  await exec('docker', ['image', 'list']);
+    const dockerfiles = matches
+      .filter((file) => {
+        const dirname = path.dirname(path.relative(rootDirectory, file));
+        return includesBy(relevantChanges, (change) => change.startsWith(dirname));
+      })
+      .map(async (file) => {
+        const filename = path.basename(file);
+        const image = filename.match(/^Dockerfile\.(.*)$/)[1];
+        const gitSHA = process.env.GITHUB_SHA;
+        const tag = `gcr.io/${project}/${image}:${gitSHA}`;
+        const cwd = path.dirname(file);
+        await exec('docker', ['build', '-f', filename, '-t', tag], { cwd });
+      });
+    await Promise.all(dockerfiles);
+    await exec('docker', ['image', 'list']);
+  } catch (error) {
+    core.setFailed(error);
+  }
 })();
