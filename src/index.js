1f847de305616b9250065a99b68326989f693ce6
Nicholas Farley <nicholasafarley@gmail.com>
2020-07-10 12:35:23 -0400

    Reorganize determination of which dockerfiles to build

--- src/index.js
+++ src/index.js
@@ -6,15 +6,21 @@ const glob = require('@actions/glob');
 (async () => {
   const rootDirectory = core.getInput('root-directory', { required: true });
   const changedFiles = core.getInput('changed-files', { required: true });
-  const dockerfiles = await glob
-    .create(path.resolve(rootDirectory, '**', 'Dockerfile.*'))
-    .then((globber) => globber.glob());
-  const contexts = JSON
-    .parse(changedFiles)
-    .map((p) => path.relative(rootDirectory, p))
-    .filter((p) => !p.startsWith('../'))
-    .map((p) => p.split('/')[0]);
+
+  const changedContexts = new Set(
+    JSON
+      .parse(changedFiles)
+      .map((p) => path.relative(rootDirectory, p))
+      .filter((p) => !p.startsWith('../'))
+      .map((p) => p.split('/')[0]),
+  );
+
+  const globber = await glob.create(path.resolve(rootDirectory, '**', 'Dockerfile.*'));
+  const matches = await globber.glob();
+
+  const dockerfiles = matches
+    .map((file) => path.relative(rootDirectory, file))
+    .filter((p) => changedContexts.has(path.dirname(p)));
 
   console.log(dockerfiles);
-  console.log(contexts);
 })();
