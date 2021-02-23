a32174433cf5ea7df1c4d48ff0b47943e1b5b546
alexrsaff <alexrsaff@gmail.com>
2021-02-23 10:02:55 -0500

    Remove old unused functionality.

--- src/index.js
+++ src/index.js
@@ -44,12 +44,10 @@ const stampStream = (stamp) => new Transform({
 const buildThenDeploy = (registry, shouldDeploy, imageTags) => async (dockerfile) => {
   const filename = path.basename(dockerfile);
   // matches 'Dockerfile' or 'Dockerfile.*', but not 'Dockerfile.'
-  const [matchError, image] = try$(() => (filename.match(/^Dockerfile(?:\.(.+))?$/)[1] || ''));
-  if (matchError) throw new Error(`${filename} points to an improperly named Dockerfile. Dockerfiles must either be 'Dockerfile' or 'Dockerfile.*'.`);
   const gitSHA = process.env.GITHUB_SHA;
   const cwd = path.dirname(dockerfile);
   const subfolder = path.basename(cwd);
-  const imageName = path.join(registry, subfolder, image);
+  const imageName = path.join(registry, subfolder);
   const stamp = `${subfolder}/${filename}`;
   const outStream = stampStream(stamp);
   outStream.pipe(process.stdout);
@@ -98,7 +96,7 @@ const main = async () => {
   const [tagParseError, parsedImageTags] = try$(() => JSON.parse(imageTags));
   if (tagParseError) return core.setFailed(`Input image-tags is not valid JSON: ${parseError}`);
 
-  const [globError, globber] = await try$(glob.create(path.resolve(root, '**', 'Dockerfile*')));
+  const [globError, globber] = await try$(glob.create(path.resolve(root, '**', 'Dockerfile')));
   if (globError) return core.setFailed(`Can't create glob for Dockerfiles: ${globError}`);
 
   const [matchError, matches] = await try$(globber.glob());
